5fe66f6b07a3594ab30021640a015aeeb77ac85c
drossano <72749793+drossano@users.noreply.github.com>
2021-08-23 18:32:17 -0400

    Add playerChoice function

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let rockValue;
 let paperValue;
 let scissorsValue;
-let computerValue;
-let playerValue;
+let computerSelect;
+let playerSelect;
 
 /* computerPlay randomly selects number from 1-3 (0-2???)
     If 1 
@@ -13,32 +13,40 @@ let playerValue;
         
     Repeat for paper and scissors*/
 function computerPlay(){
-    let computerSelectString;
+    
     let computerSelect = Math.floor(Math.random()*3);
-    if (computerSelect == 0) {
-        computerSelectString = "Rock";
+    if (computerSelect == 0) { //Rock
         scissorsValue = 1;
         rockValue = 2;
         paperValue = 3;
         computerSelect = rockValue;
-    }else if (computerSelect == 1) {
-        computerSelectString = "Paper";
+    }else if (computerSelect == 1) { //Paper
         rockValue = 1;
         paperValue = 2;
         scissorsValue = 3;
         computerSelect = paperValue;
-    }else if (computerSelect ==2) {
-        computerSelectString = "Scissors"
+    }else if (computerSelect ==2) { //Scissors
         paperValue = 1;
         scissorsValue = 2;
         rockValue = 3;
         computerSelect = scissorsValue;
     }
+    return rockValue;
 }
 
 //Prompt player to pick rock paper or scissors 
     //Assign associated value with players choice
-
+function playerChoice() {
+    const playerSelectString = prompt("You're challenged to a game of Rock Paper Scissors! Enter \"Rock\" \"Paper\" or \"Scissors\" to play!")
+    if (playerSelectString.toLowerCase() == "rock") {
+        playerSelect = rockValue;
+    } else if (playerSelectString.toLowerCase() == "paper") {
+        playerSelect = paperValue;
+    } else if (playerSelectString.toLowerCase() == "scissors") {
+        playerSelect = scissorsValue;
+    }
+    return playerSelect;
+}
 /*Compare player and computer choices
     If player> computer
         hen player wins
